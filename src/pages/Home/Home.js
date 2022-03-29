3f0a91a2334c0318e994835905f4f848d58aa3da
Sethvan <sethvan@yahoo.com>
2022-03-29 09:44:47 -0700

    added hamburger to Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,6 @@
-import react, { Component } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-
 
 class Home extends Component {
   render() {
@@ -13,18 +12,53 @@ class Home extends Component {
               <h3 className="float-md-left mb-0">
                 Iglesia Cristiana Bautista Sinaí
               </h3>
-              <nav className="nav nav-masthead justify-content-center float-md-right">
-                <Link className="nav-link active" aria-current="page" to="#">
-                  Inicio
-                </Link>
-                <Link className="nav-link" to="/quienes_somos">
-                  Conocenos
-                </Link>
+
+              <nav className="nav nav-masthead navbar-expand-xl navbar-dark justify-content-center float-md-right">
+                <button
+                  id="hamburger"
+                  className="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarHome"
+                  aria-controls="navbarHome"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                  className="collapse navbar-collapse navbar-expand-lg"
+                  id="navbarHome"
+                >
+                  <div className="navbar-nav ">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="#"
+                    >
+                      Inicio
+                    </Link>
+                    <Link className="nav-link" to="/quienes_somos">
+                      Conocenos
+                    </Link>
+                    <Link className="nav-link" to="/estudios">
+                      Enseñanzas
+                    </Link>
+                    <Link className="nav-link" to="/reuniones">
+                      Reuniones
+                    </Link>
+                    <Link className="nav-link" to="/eventos">
+                      Eventos y Anuncios
+                    </Link>
+                  </div>
+                </div>
               </nav>
             </div>
           </header>
           <main className="px-3">
-            <h1>¡Bienvenidos a la Iglesia Cristiana Bautista Sinaí!</h1>
+            <h1 className="mt-3">
+              ¡Bienvenidos a la Iglesia Cristiana Bautista Sinaí!
+            </h1>
             <p className="lead">
               "Nosotros lo amamos a él porque él nos amó primero.""
               <br />- 1 Juan 4:19
